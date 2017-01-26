c89b4e07ca5944c876fe34b9e820ebdfce05f74a
Belen Fuster <mbfuster@uc.cl>
2017-01-26 01:09:17 -0300

    pequeño avance

--- Actividades/AC01/main.py
+++ Actividades/AC01/main.py
@@ -16,7 +16,7 @@ class Sucursal:
         self.usados = usados
 
     def AgregarAuto(self, auto):
-        if not auto.estado:
+        if auto.estado == "Usado" or auto.estado=="usado":
             self.usados.append(auto)
         else:
             self.nuevos.append(auto)
@@ -39,17 +39,58 @@ class Contacto:
 
 a = Automotora("lhosgdsd")
 print(a)
-b = Sucursal("qwerty",[],[])
-a.crear_sucursal(b)
-c= Auto('001', "toyota", 2009,"jkhkg", "MT", 12000000, estado = "Nuevo")
-p = Contacto("belen", "18466323-6", "73395180", "mbfuster@uc.cl")
-d=Auto("002","qwr",2012,"oiu{p","MT",15000000, "Usado", p)
-print(d.dueno.nombre)
+b1 = Sucursal("LasCondes",[],[])
+b2 = Sucursal("Vitacura",[],[])
+b3 = Sucursal("Ñuñoa",[],[])
+b4 = Sucursal("Providencia",[],[])
+a.crear_sucursal(b1)
+a.crear_sucursal(b2)
+a.crear_sucursal(b3)
+a.crear_sucursal(b4)
+
+p1 = Contacto("belen", "18466323-6", "73395180", "mbfuster@uc.cl")
+p2 = Contacto("Juan","72440145","76543210","juanin@31min.com")
+
+c1= Auto('001', "toyota", 2009,"jkhkg", "MT", 12000000, estado = "Nuevo")
+c2=Auto("002","qwr",2012,"oiu{p","MT",15000000, "Usado", p1)
+c3=Auto("003","honda",2013,"kshgo","AT",13500000,estado="Nuevo")
+c4=Auto("004","audi",2011,"A3","MT",20000000,"Usado",p2)
+print(c2.dueno.nombre)
+
+
+#filtro para año y precio, donde parametro ("param") va a corresponder a "año" o a "precio"
+def FiltroPorValor(v_max,v_min,automotora,param):
+    lista_autos = []
+    if param == "año":
+        a = aut.year
+    else:
+        a = aut.precio
+    for suc in automotora.sucursales:
+        lista_temp = []
+        lista_temp.append(suc.nombre)
+        for aut in suc.nuevos:
+            if a <= v_max and a >= v_min:
+                lista_temp.append(aut)
+        for aut in suc.usados:
+            if a <= v_max and a >= v_min:
+                lista_temp.append(aut)
+        lista_autos.append(lista_temp)
+        lista_temp = []
+        return lista_autos
+
+#def FiltroPorMarca(marca,automotora):
+#    for suc in
+
+
 
 while True:
     r1 = input("trabajador o cliente?")
     if r1 == "cliente" or "Cliente":
+        lista_cliente=[]
         r2c = int(input("¿Qué estás buscando? \n1:Rango de años\n2:Rango de precio\n3:Marca\n4:Tipo de transmision\n5:Usado o nuevo\n"))
         if r2c == 1:
             ano_min=int(input("desde que año"))
             ano_max=int(input("hasta que año"))
+            l1=FiltroPorValor(ano_max,ano_min,a,"año")
+
+
